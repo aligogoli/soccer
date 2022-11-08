6987bb399eb61fbddc747d474965b0ce22dcf538
aliquazaimodo <ajali4487@gmail.com>
2022-11-08 22:27:14 +0330

    soccer.py
    it's only a practice

---
+++ soccer.py
@@ -0,0 +1,11 @@
+print('Hello soccer fans')
+username= input('username:')
+passwordsite = int(input('password:'))
+if passwordsite == 2233 and username == 'admin':
+  tb = input('who is the best player in the world last year:')
+  if tb == ('Karim Benzema'):
+   print('yes you done it ')
+  else:
+    print('you made mistake and you are so silly and you should watch Kartoon')
+
+
